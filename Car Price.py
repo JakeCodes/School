2416315926c6892bf8a816bb401022e044b804b9
JakeCodes <jakethesnake20006@gmail.com>
2021-09-09 20:54:36 -0500

    added constant value

--- Car Price.py
+++ Car Price.py
@@ -1,5 +1,5 @@
 # base price
-basePrice = input("What is your base car price? ")
+basePrice = float(input("What is your base car price? "))
 
 # additional charges
 tax = float(input("tax: "))
@@ -7,7 +7,9 @@ license = float(input("license: "))
 dealerPrep = float(input("dealer prep: "))
 destinationCharge = float(input("destination charge: "))
 
-total = round(tax + license + dealerPrep + destinationCharge + basePrice)
+const = 1.1245
+
+total = round((tax + license + dealerPrep + destinationCharge + basePrice)* const)
 
 print(total)
 
